82ca9919d8f720a4dfae1cf62437eb5c90dbb76e
s0a1e2 <oops2198@gmail.com>
2023-12-20 09:02:02 +0330

    update DTO's

--- src/dto/create-problem.dto.ts
+++ src/dto/create-problem.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProblemDto {}
+import { IsString } from 'class-validator';
+
+export class CreateProblemDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  creator: string;
+}
--- src/dto/update-problem.dto.ts
+++ src/dto/update-problem.dto.ts
@@ -1,6 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateProblemDto } from './create-problem.dto';
+import { ObjectId } from 'mongodb';
 
-export class UpdateProblemDto extends PartialType(CreateProblemDto) {
-  id: number;
+export class UpdateProblemDto extends PickType(CreateProblemDto, [
+  'description',
+  'title' as const,
+]) {
+  id: ObjectId;
 }
