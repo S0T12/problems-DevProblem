a0ede2174d67a411c9ebbf0ad6b20f4219f6de2e
s0a1e2 <oops2198@gmail.com>
2024-01-03 02:36:26 +0330

    import users service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { ProblemsController } from './problems.controller';
 import { ProblemsService } from './problems.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Problem, ProblemSchema } from './schemas/problem.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'users_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
     MongooseModule.forRoot('mongodb://localhost:27017/problems-DevProblem'),
     MongooseModule.forFeature([{ name: Problem.name, schema: ProblemSchema }]),
   ],
