80f635cdd18f21a19a7753c0bee49144154d2eb9
s0a1e2 <oops2198@gmail.com>
2024-01-04 00:01:51 +0330

    config problems service

--- src/problems.service.ts
+++ src/problems.service.ts
@@ -1,36 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateProblemDto } from './dto/create-problem.dto';
 import { UpdateProblemDto } from './dto/update-problem.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Problem } from './schemas/problem.schema';
 import { Model } from 'mongoose';
 import { ObjectId } from 'mongodb';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class ProblemsService {
-  constructor(@InjectModel(Problem.name) private ProbmeModel: Model<Problem>) {}
-  async create(createProblemDto: CreateProblemDto) {
+  constructor(
+    @InjectModel(Problem.name) private ProblemModel: Model<Problem>,
+    @Inject('USERS_SERVICE') private readonly client: ClientProxy,
+  ) {}
+  async create(createProblemDto: CreateProblemDto): Promise<Problem> {
     try {
-      const problem = new this.ProbmeModel(createProblemDto);
-      await problem.save();
+      const problem = new this.ProblemModel(createProblemDto);
+      return await problem.save();
     } catch (error) {
-      throw error;
+      throw new BadRequestException(error.message);
     }
   }
 
   async findAll() {
-    return await this.ProbmeModel.find().exec();
+    const result = await this.ProblemModel.find();
+    return result;
   }
 
   async findOne(id: ObjectId) {
-    return await this.ProbmeModel.findById(id).exec();
+    const result = await this.ProblemModel.findById(id);
+    return result;
   }
 
   async update(id: ObjectId, updateProblemDto: UpdateProblemDto) {
-    return await this.ProbmeModel.updateOne(id, updateProblemDto).exec();
+    const result = await this.ProblemModel.updateOne(id, updateProblemDto);
+    return result;
   }
 
   async remove(id: ObjectId): Promise<any> {
-    return await this.ProbmeModel.deleteOne(id);
+    const result = await this.ProblemModel.deleteOne(id);
+    return result;
   }
 }
